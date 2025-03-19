ad012381e0b043c4a9e7e623197ac9c7df75f90b
Samuel Rodriguez Paez <alonsopae1206@gmail.com>
2025-03-19 22:36:21 +0100

    fixed37

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   optimizeDeps: {
     include: ["three"]
+  },
+  build: {
+    assetsInlineLimit: 0, // Evita que los assets grandes sean transformados
   }
 });
